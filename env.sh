a9a6e22405f3b5a1290b5ece79f927df9290a998
Riichi Rusdiana <noreply@riichi.my.id>
2024-12-18 18:32:52 +0700

    update: fzf

--- env.sh
+++ env.sh
@@ -38,6 +38,18 @@ export PATH="$HOME/.local/bin:$PATH"
 # Exporting Rust Cargo's binary PATH
 export PATH="$HOME/.cargo/bin:$PATH"
 
+# Exporting FZF default configuration
+export FZF_DEFAULT_COMMAND="fd --hidden --strip-cwd-prefix --exclude .git "
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export FZF_ALT_C_COMMAND="fd --type d --hidden --strip-cwd-prefix --exclude .git "
+
+export FZF_DEFAULT_OPTS="--height 50% --layout default --border --color=hl:#2dd4bf"
+
+export FZF_TMUX_OPTS=" -p90%,70% "
+
+export FZF_CTRL_T_OPTS="--preview 'bat --color=always -n --line-range :500 {}'"
+export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
+
 # NOTE: Exports used by libraries
 #
 # These exports are essentials for development.
